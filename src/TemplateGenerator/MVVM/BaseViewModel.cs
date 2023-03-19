3635b848f2cb4f36f98d63923328b8eb8bf6e297
RSmartApps <rubensa87@hotmail.es>
2023-03-19 15:14:41 +0100

    Add processses selection

---
+++ src/TemplateGenerator/MVVM/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TemplateGenerator.MVVM
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public bool SetPropperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            field = value;
+            return PropertyChanged != null;
+        }
+        #endregion
+    }
+}
---
+++ src/TemplateGenerator/MVVM/ProgramPickerVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TemplateGenerator.MVVM
+{
+    public class ProgramPickerVM : BaseViewModel
+    {
+        #region fields
+        private List<Process> _Services;
+        private Process _SelectedService;
+        #endregion
+        #region propperties
+        public Process SelectedService { get { return _SelectedService; } set { SetPropperty(ref _SelectedService, value, nameof(_SelectedService)); } }
+        public List<Process> Services { get { return _Services; } set { SetPropperty(ref _Services, value, nameof(_Services)); } }
+        #endregion
+
+        public ProgramPickerVM()
+        {
+            Services = Process.GetProcesses().ToList();
+        }
+    }
+}
