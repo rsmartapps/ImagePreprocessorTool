26f62974c65b8e8f9b9590cc228d6019b6a8d6ea
rsmartapps <anger32@Hotmail.com>
2024-06-13 18:39:53 +0200

    Feature/select process (#2)
    * use material design as theme
    
    * Implementation
    
    * comment old tests
    
    ---------
    
    Co-authored-by: Rubén Sebastian <Ruben.Sebastian@dnv.com>

---
+++ src/TemplateCreator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace TemplateCreator;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow(MainWindowViewModel viewModel)
+    {
+        DataContext = viewModel;
+        InitializeComponent();
+
+        CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, OnClose));
+    }
+
+    private void OnClose(object sender, ExecutedRoutedEventArgs e)
+    {
+        Close();
+    }
+}
+public partial class MainWindowViewModel : ObservableObject
+{
+    public MainWindowViewModel()
+    {
+        Processes = Process.GetProcesses().OrderBy(p => p.ProcessName).ToList();
+    }
+    [ObservableProperty]
+    private List<Process> _processes;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsProcessSelected))]
+    private Process _selectedProcess;
+
+    public bool IsProcessSelected => SelectedProcess != null;
+}
