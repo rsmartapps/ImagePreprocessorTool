4b2f75ac3ae6b0c7fa0bb605d8f10bfe8ed0f97a
Rubén Sebastian <Ruben.Sebastian@dnv.com>
2024-06-13 15:19:27 +0200

    use material design as theme

---
+++ src/TemplateCreator/TemplateCreator/TemplateCreator/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace TemplateCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow(MainWindowViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, OnClose));
+        }
+
+        private void OnClose(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
